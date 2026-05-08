755c4a16d2281e84c4a92b3333e28703f48acb3d
Akbar3776 <akbar3776@gmail.com>
2026-05-08 21:30:14 +0700

    Pisah config database dan Jwt secret key ke .env

--- magang_platform/app.py
+++ magang_platform/app.py
@@ -3,14 +3,22 @@ from flask import Flask
 from models import db
 from routes import api
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/careerflow'
+# Tambahkan konfigurasi database dan JWT dari environment variables
+# Buat file .env di root project dengan isi:
+# DATABASE_URL=postgresql://username:password@localhost:5432/nama_database
+# JWT_SECRET_KEY=string_rahasia_untuk_jwt (ganti dengan string acak yang lebih kuat di tahap produksi)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Tambahkan Secret Key untuk JWT (ganti dengan string acak yang lebih kuat di tahap produksi)
-    app.config['JWT_SECRET_KEY'] = 'Fauzan-Hani-Ibnu-Projek-CareerFlow'
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 
 #Setup Database GUNAKAN VIRTUAL ENVIRONMENT
@@ -20,7 +28,7 @@ def create_app():
 
     db.init_app(app)
     jwt = JWTManager(app) # Inisialisasi JWT
-    
+
     app.register_blueprint(api)
 
     with app.app_context():
