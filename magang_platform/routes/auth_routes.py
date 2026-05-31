674b5abf14c7420d9058786243f3d51520545d4f
Akbar3776 <akbar3776@gmail.com>
2026-05-31 18:45:54 +0700

    Fix: refactor auth_routes.py

--- magang_platform/routes/auth_routes.py
+++ magang_platform/routes/auth_routes.py
@@ -4,19 +4,19 @@ from controllers.auth_controller import AuthController
 
 auth_bp = Blueprint('auth', __name__)
 
-# Sign Up & Login
-auth_bp.route('/signup', methods=['POST'])(AuthController.signup)
-auth_bp.route('/login', methods=['POST'])(AuthController.login)
+# Sign Up & Login (Added 'OPTIONS' to both)
+auth_bp.route('/signup', methods=['POST', 'OPTIONS'])(AuthController.signup)
+auth_bp.route('/login', methods=['POST', 'OPTIONS'])(AuthController.login)
 
-# OTP Send & Verify
-auth_bp.route('/signup/send-otp', methods=['POST'])(AuthController.send_signup_otp)
-auth_bp.route('/signup/verify-otp', methods=['POST'])(AuthController.verify_signup_otp)
+# OTP Send & Verify (Added 'OPTIONS' to both)
+auth_bp.route('/signup/send-otp', methods=['POST', 'OPTIONS'])(AuthController.send_signup_otp)
+auth_bp.route('/signup/verify-otp', methods=['POST', 'OPTIONS'])(AuthController.verify_signup_otp)
 
-# Forgot Password
-auth_bp.route('/forgot-password/send-otp', methods=['POST'])(AuthController.send_forgot_otp)
-auth_bp.route('/forgot-password/verify-otp', methods=['POST'])(AuthController.verify_forgot_otp)
-auth_bp.route('/forgot-password/reset', methods=['POST'])(AuthController.reset_password)
+# Forgot Password (Added 'OPTIONS' to all)
+auth_bp.route('/forgot-password/send-otp', methods=['POST', 'OPTIONS'])(AuthController.send_forgot_otp)
+auth_bp.route('/forgot-password/verify-otp', methods=['POST', 'OPTIONS'])(AuthController.verify_forgot_otp)
+auth_bp.route('/forgot-password/reset', methods=['POST', 'OPTIONS'])(AuthController.reset_password)
 
-# Profile
-auth_bp.route('/profile', methods=['GET'])(jwt_required()(AuthController.get_profile))
-auth_bp.route('/profile', methods=['PUT'])(jwt_required()(AuthController.update_profile))
+# Profile (Added 'OPTIONS' to both GET and PUT)
+auth_bp.route('/profile', methods=['GET', 'OPTIONS'])(jwt_required()(AuthController.get_profile))
+auth_bp.route('/profile', methods=['PUT', 'OPTIONS'])(jwt_required()(AuthController.update_profile))
